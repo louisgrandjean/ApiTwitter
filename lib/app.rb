1362825f023351fb3484486c5f37098e040d4678
Neeiyo <louis.grandjean99@gmail.com>
2021-01-20 09:41:22 +0100

    Basses

---
+++ lib/app.rb
@@ -0,0 +1,8 @@
+require 'dotenv'# Appelle la gem Dotenv
+
+Dotenv.load('.env') # Ceci appelle le fichier .env (situé dans le même dossier que celui d'où tu exécute app.rb)
+# et grâce à la gem Dotenv, on importe toutes les données enregistrées dans un hash ENV
+
+# Il est ensuite très facile d'appeler les données du hash ENV, par exemple là je vais afficher le contenu de la clé TWITTER_API_SECRET
+puts ENV['TWITTER_API_SECRET']
+
